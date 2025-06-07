796347be531881de290f2bc38c24b35d2b7e1a7f
hyeonu <hyeonu9831@gmail.com>
2025-06-08 00:07:53 +0900

    맵 그리기 및 블럭 하나 그리기 구현 완료

--- tetris.c
+++ tetris.c
@@ -28,8 +28,6 @@
 #define	T_BLOCK 1
 #define S_BLOCK 2
 #define Z_BLOCK 3
-
-
 #define L_BLOCK 4
 #define J_BLOCK 5
 #define O_BLOCK 6
@@ -157,7 +155,14 @@ int best_point = 0; /* 최고 점수*/
 long point = 0; /* 현재 점수*/
 
 
+
 int display_menu(void); /* 메뉴 표시*/
+
+void draw_table(void);
+char (*get_block_shape(int num))[4][4];
+void draw_block(int block_num, int state, int x, int y);
+int game_start(void);
+
 
 
 
@@ -168,7 +173,7 @@ int display_menu(void); /* 메뉴 표시*/
 /* 메뉴 표시*/
 int display_menu(void)
 {
-	// 
+	//
 	int menu = 0;
 
 	while(1)
@@ -197,12 +202,132 @@ int display_menu(void)
 	return 0;
 }
 
+/**
+ * 전체 테트리스 맵을 그리는 함수
+ * - 좌우 벽과 바닥 테두리 생성
+ * - 현재 tetris_table 배열 값을 기반으로 출력
+ */
+void draw_table(void)
+{
+    int i, j;
+    // 테두리 및 빈 공간 초기화
+    for (i = 0; i < 21; i++) {
+        for (j = 0; j < 10; j++) {
+            if (j == 0 || j == 9 || i == 20)
+                tetris_table[i][j] = 1;  // 테두리
+            else
+                tetris_table[i][j] = 0;  // 빈 공간
+        }
+    }
 
+    // 화면 클리어
+    system("clear");
+
+    // 테이블 출력
+    for (i = 0; i < 21; i++) {
+        for (j = 0; j < 10; j++) {
+            if (i == 20) {
+                // 바닥
+                printf("_");
+            } else if (j == 0 || j == 9) {
+                // 좌우 벽
+                printf("|");
+            } else {
+                // 빈 공간
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+/**
+ * 블록 유형에 맞는 4×4 배열 포인터를 반환
+ */
+char (*get_block_shape(int num))[4][4]
+{
+    switch (num) {
+        case I_BLOCK: return i_block;
+        case T_BLOCK: return t_block;
+        case S_BLOCK: return s_block;
+        case Z_BLOCK: return z_block;
+        case L_BLOCK: return l_block;
+        case J_BLOCK: return j_block;
+        case O_BLOCK: return o_block;
+        default:       return i_block;
+    }
+}
+
+/**
+ * 현재 블록을 그리는 함수
+ * ANSI escape 코드를 사용하여 터미널 커서 위치 이동 후 '#' 출력
+ */
+void draw_block(int block_num, int state, int x, int y)
+{
+    char (*block)[4][4] = get_block_shape(block_num);
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (block[state][i][j]) {
+                // 터미널 커서 이동: 행 y+i+1, 열 x+j+1
+                printf("\033[%d;%dH#", y + i + 1, x + j + 1);
+            }
+        }
+    }
+    fflush(stdout);
+}
+
+/// 게임 시작 핵심 로직 함수
+int game_start(void)
+{
+    // 초기 게임 설정
+    srand(time(NULL));
+    block_number     = rand() % 7;
+    next_block_number = rand() % 7;
+    block_state      = 0;
+    x                 = 3;
+    y                 = 0;
+    point             = 0;
+    game              = GAME_START;
+
+    // 게임 루프
+    while (game == GAME_START) {
+         // 1) 전체 맵 그리기 (추후 구현)
+         draw_table();
+
+        // 2) 현재 블록 그리기 (추후 구현)
+        draw_block(block_number, block_state, x, y);
+
+        // 3) 입력 비차단 처리 (추후 구현)
+        // int key = getch_noblock();
+        // if (key != -1) handle_input(key);
+
+        // 4) 자동 낙하 처리 (추후 구현)
+        // if (time_exceeded()) {
+        //     if (!move_block(DOWN)) {
+        //         fix_block();
+        //         clear_lines();
+        //         block_number     = next_block_number;
+        //         next_block_number = rand() % 7;
+        //         block_state      = 0;
+        //         x                 = 3;
+        //         y                 = 0;
+        //         if (collision()) {
+        //             game = GAME_END;
+        //         }
+        //     }
+        // }
+
+        // 5) 프레임 제어
+        usleep(50000); // 50ms 대기
+    }
+
+    return 0;
+}
 
 /// 테트리스 게임 메인 함수
 /// 메뉴를 표시하고 사용자의 선택에 따라 게임을 시작하거나 결과를 검색하거나 종료합니다.
-/// @param  
-/// @return 
+/// @param
+/// @return
 int main(void)
 {
 	int menu = 1;
@@ -218,11 +343,11 @@ int main(void)
 		}
 		else if(menu == 2)
 		{
-			search_result();
+			//search_result();
 		}
 		else if(menu == 3)
 		{
-			print_result();
+			//print_result();
 		}
 		else if(menu == 4)
 		{
@@ -232,3 +357,5 @@ int main(void)
 
 	return 0;
 }
+
+
